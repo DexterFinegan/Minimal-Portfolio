d55fa1a05567fb42d57087f577357fd28fab6191
DexterFinegan <dexter.finegan@gmail.com>
2024-10-13 13:46:01 +0100

    End of Shooter Tutorial
    Finished the tutorial for shooter game, fully functional, plan to add more and make it my own

--- Shooter/shooter.js
+++ Shooter/shooter.js
@@ -4,6 +4,11 @@ const c = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const scoreEl = document.querySelector("#scoreEl")
+const startGameBtn = document.querySelector("#startGameBtn")
+const modlaEl = document.querySelector("#modalEl")
+const bigScoreEl = document.querySelector("#bigScoreEl")
+
 class Player {
     constructor (x, y, radius, colour) {
         this.x = x
@@ -66,12 +71,54 @@ class Enemy {
     }
 }
 
+const friction = 0.98
+class Particle {
+    constructor (x, y, radius, colour, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.colour = colour
+        this.velocity = velocity
+        this.alpha = 1
+    }    
+
+    draw() {
+        c.save()
+        c.globalAlpha = this.alpha
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false)
+        c.fillStyle = this.colour
+        c.fill()
+        c.restore()
+    }
+
+    update() {
+        this.draw()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, "blue")
-const projectiles = []
-const enemies = []
+let player = new Player(x, y, 10, "white")
+let projectiles = []
+let enemies = []
+let particles = []
+
+function init () {
+    player = new Player(x, y, 10, "white")
+    projectiles = []
+    enemies = []
+    particles = []
+    score = 0
+    scoreEl.innerHTML = score
+    bigScoreEl.innerHTML = score
+}
 
 function spawnEnemies() {
     setInterval(() => {
@@ -86,7 +133,7 @@ function spawnEnemies() {
             x = Math.random() *canvas.width
             y = Math.random() < 0.5 ? - radius : canvas.height + radius
         }
-        const colour = "green"
+        const colour = `hsl(${Math.random()*360}, 50%, 50%)`
         const angle = Math.atan2(canvas.height/2 - y, canvas.width/2 - x)
         const velocity = {x : Math.cos(angle), y : Math.sin(angle)}
     
@@ -94,27 +141,78 @@ function spawnEnemies() {
     }, 1000)
 }
 
-spawnEnemies()
-
+let animationID
+let score = 0
 function animate(){
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    animationID = requestAnimationFrame(animate)
+    c.fillStyle = "rgba(0, 0, 0, 0.1)"
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach((projectile) => {
+    particles.forEach((particle, index) => {
+        if (particle.alpha <= 0) {
+            particles.splice(index, 1)
+        } else{
+            particle.update()
+        }
+    })
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+        
+        // remove from edges of screen
+        if (projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width||
+            projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
     })
 
     enemies.forEach((enemy, index) => {
         enemy.update()
 
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+        if (dist -enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationID)
+            modlaEl.style.display = "flex"
+            bigScoreEl.innerHTML = score
+        }
+
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-
+            
+            // when projectile touch enemy
             if (dist -enemy.radius - projectile.radius < 1) {
-                setTimeout(() => {
-                    enemies.splice(index, 1)
-                    projectiles.splice(projectileIndex, 1)
-                }, 0)
+
+                // create explosions
+                for (let i = 0; i < enemy.radius*2; i++) {
+                    particles.push(new Particle(projectile.x, projectile.y, Math.random()*2, enemy.colour, {
+                        x : (Math.random() - 0.5)*(Math.random() * 6),
+                         y : (Math.random() - 0.5)*(Math.random() * 6)
+                        }
+                    )
+                )
+                }
+
+                if (enemy.radius - 10 > 5) {
+                    // increase score
+                    score += 100
+                    scoreEl.innerHTML = score
+                    gsap.to(enemy, {
+                        radius : enemy.radius - 10
+                    })
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                } else{
+                    // increase score
+                    score += 250
+                    scoreEl.innerHTML = score
+
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                }
             }
         })
     })
@@ -122,8 +220,19 @@ function animate(){
 
 addEventListener("click", (event) => {
     const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2)
-    const velocity = {x : Math.cos(angle), y : Math.sin(angle)}
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height /2, 5, "red", velocity))
+    const velocity = {x : Math.cos(angle) * 4, y : Math.sin(angle) * 4}
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height /2, 5, "white", velocity))
 })
 
-animate()
+addEventListener("touchstart", (event) => {
+    const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2)
+    const velocity = {x : Math.cos(angle) * 4, y : Math.sin(angle) * 4}
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height /2, 5, "white", velocity))
+})
+
+startGameBtn.addEventListener("click", () => {
+    init()
+    animate()
+    spawnEnemies()
+    modlaEl.style.display = "none"
+})
