bc3d5c5b9faac6ef366cac84fc440806c0d2c3b7
DexterFinegan <dexter.finegan@gmail.com>
2024-10-22 12:21:45 +0100

    Beginning of Quiz Game
    Remodelled server and code from multiplayer game to a base unit to begin quiz game, not much but difficult to understand

---
+++ Quiz Game/public/js/frontend.js
@@ -0,0 +1,27 @@
+
+const socket = io()
+
+const frontEndPlayers = {}
+
+socket.on("updatePlayers", (backEndPlayers) => {
+  for (const id in backEndPlayers) {
+    const backEndPlayer = backEndPlayers[id]
+
+    if (!frontEndPlayers[id]) {
+      frontEndPlayers[id] = new Player({})
+      }
+  }
+
+  // this is where we delete frontend players
+  for (const id in frontEndPlayers) {
+    if (!backEndPlayers[id]) {
+      delete frontEndPlayers[id]
+    }
+  }
+})
+
+document.querySelector("#usernameForm").addEventListener("submit", (event) => {
+  event.preventDefault()
+  document.querySelector("#usernameForm").style.display = "none"
+  socket.emit("initGame", {username : document.querySelector("#usernameInput").value})
+})
